516cda54cb7c2a5bda4c813b8b71e289b5ee09e2
glin <guo-shiuan.lin@epfl.ch>
2023-10-26 12:50:13 +0200

    create structure

---
+++ HW2/src/main.cc
@@ -0,0 +1,12 @@
+#include <cstdlib>
+#include <iostream>
+/* -------------------------------------------------------------------------- */
+
+int main(int argc, char **charv) {
+
+  int N = atoi(charv[1]);
+
+  std::cout << "Hello " << N << " res " << std::endl;
+
+  return EXIT_SUCCESS;
+}
