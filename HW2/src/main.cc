eaa37d53f877f22623a2f6eeabc5ea1724ea0e08
glin <guo-shiuan.lin@epfl.ch>
2023-10-26 15:42:19 +0200

    done exercise 1 and 2

--- HW2/src/main.cc
+++ HW2/src/main.cc
@@ -1,12 +1,24 @@
-#include <cstdlib>
 #include <iostream>
-/* -------------------------------------------------------------------------- */
+#include "series.hh"
 
-int main(int argc, char **charv) {
+int main(int argc, char *argv[]) {
+    int N = std::atoi(argv[1]); //N: the maximum iterator
+    int M = std::atoi(argv[2]); //M: if 0: ComputeArithmetic. if 1: Compute Pi
 
-  int N = atoi(charv[1]);
+    if (N <= 0) {
+        std::cout << "N should be a positive integer." << std::endl;
+    } else {
+      if (M==0){
+        ComputeArithmetic numberSum;
+        double result = numberSum.compute(N);
+        std::cout << "The sum of numbers from 1 to " << N << " is: " << result << std::endl;
+      }
+      else if (M==1){
+        ComputePi numberSum;
+        double result = numberSum.compute(N);
+        std::cout << "Pi convergene using integers from 1 to " << N << " is: " << result << std::endl;
+      }
+    }
 
-  std::cout << "Hello " << N << " res " << std::endl;
-
-  return EXIT_SUCCESS;
+    return 0;
 }
---
+++ HW2/src/series.hh
@@ -0,0 +1,50 @@
+#ifndef SERIES_HH
+#define SERIES_HH
+
+#include <iostream>
+#include <cmath>
+
+// parent class
+class Series{
+public:
+virtual double compute(unsigned int N) = 0;
+};
+
+// child class that computes the sum of 1,2,..., N
+class ComputeArithmetic : public Series {
+public:
+    // Constructor to initialize N
+    ComputeArithmetic() {
+    }
+
+    // Method to calculate and return the sum of numbers from 1 to N
+    double compute(unsigned int N) override {
+        double sum = 0.0;
+        for (unsigned int i = 1; i <= N; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+};
+
+// child class that computes the convergence of Pi
+class ComputePi : public Series {
+public:
+    // Constructor to initialize N
+    ComputePi() {
+    }
+
+    // Method to calculate and return Pi convergence using integers from 1 to N
+    double compute(unsigned int N) override {
+        float pi = 0.;
+        for (unsigned int i = 1; i <= N; i++) {
+             pi += 1./(1.*i*i);
+        }
+        pi *= 6.;
+        pi = sqrt(pi);
+        return pi;
+    }
+
+};
+
+#endif // SERIES_HH
