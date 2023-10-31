02373f6aba934cd2f8f2085948f8ad75c829ec51
Zanavje2411 <148066569+Zanavje2411@users.noreply.github.com>
2023-10-31 16:31:05 +0100

    Add files via upload
    Uploaded main file with integrated Excercise 6.

--- HW2/src/main.cc
+++ HW2/src/main.cc
@@ -1,7 +1,15 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>      
+#include <cstring>
+#include <string>
+#include <memory>
+#include <fstream>
+#include <cmath>
 #include "series.hh"
 #include "dumper.hh"
+#include "RiemannIntegral.hh"
+
 
 int main(int argc, char *argv[]) {
     // int N = std::atoi(argv[1]); //N: the maximum iterator
@@ -46,7 +54,58 @@ int main(int argc, char *argv[]) {
       // PrintSeries object(freq, N, numberSum);
       // object.dump();
     }
+    else if (M==2){ //To calculate RiemannIntegral using the main file
+      std::string functionType;
 
+      std::cout <<"Enter the lower bound (a):";
+      double lowerBound;
+      std::cin >> lowerBound;
+
+      std::cout <<"Enter the upper bound (b):";
+      double upperBound;
+      std::cin >> upperBound;
+
+      std::cout << "Choose the function type ('cube', 'cos', or 'sin'): ";
+      std::cin >> functionType;   
+
+      //Define the function to be integrated 
+      std::function<double(double)>function;
+
+      if (functionType == "cube")
+      {
+        //Case 1 - f(x)=x^3
+        auto calcCube = [](double x) {return 1.0 * x * x * x; };
+        function = calcCube;
+      }
+      else if (functionType == "cos") {
+      //Case 2 - f(x)=cos(x)
+      auto cosine = [](double x) {return std::cos(x); };
+      function = cosine;
+      }
+      else if (functionType == "sin"){
+      //Case 3 - f(x)=sin(x)
+      auto sin = [](double x) {return std::sin(x); }; 
+      function = sin;
+      }
+      else {
+      std::cout << "Invalid function type. Choose 'cube', 'cos', or 'sin'."<<std::endl;
+      return 1; //Exit with and error code
+      }
+
+      int numIntervals;
+      std::cout <<"Enter the number of intervals:";
+      std::cin >> numIntervals;
+
+      //Create a Riemann Integral object
+
+      RiemannIntegral riemann (function, lowerBound, upperBound, numIntervals);
+      //Calculate the Riemann sum
+
+      double result = riemann.CalculateRiemannSum();
+      std::cout <<"Integral of function" <<functionType << "from" <<lowerBound<< "to" <<upperBound << ":" << result << std::endl;
+
+    }
+     
   double result = numberSum->compute(N);
   std::cout << result << std::endl;
 
