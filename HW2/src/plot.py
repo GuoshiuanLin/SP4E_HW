ecaadfe52ad515edbf47ddc58861d1c626f4be94
GuoshiuanLin <135812841+GuoshiuanLin@users.noreply.github.com>
2023-11-01 13:50:55 +0100

    Update plot.py

--- HW2/src/plot.py
+++ HW2/src/plot.py
@@ -2,7 +2,7 @@ from matplotlib import pyplot as plt
 import pandas as pd
 import os
 
-# check the existent file name, read by pandas, and plot the numerical results on y axis with the iteration on x axis
+# check the existent file name, read by pandas
 if os.path.isfile('output.csv'):
     file = pd.read_csv('output.csv', index_col=False, names=['numbers'])
 elif os.path.isfile('output.txt'):
@@ -11,6 +11,7 @@ else:
     file = pd.read_csv('output.psv',index_col=False, names=['numbers'],sep='|')
     
 plt.figure()
+# plot the numerical results on y axis with the iteration on x axis
 plt.plot(file.index[:-1], file.numbers[:-1], '-^')
 plt.xlabel('iterations')
 plt.ylabel('sereis results')
