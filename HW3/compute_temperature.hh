6767562f8d208dc9150866afd2e85485976c17c1
Zanavje2411 <148066569+Zanavje2411@users.noreply.github.com>
2023-12-01 17:20:47 +0100

    Add files via upload
    Updated material_point.hh 
    
    Updated ComputeTemperature class

---
+++ HW3/compute_temperature.hh
@@ -0,0 +1,29 @@
+#ifndef __COMPUTE_TEMPERATURE__HH__
+#define __COMPUTE_TEMPERATURE__HH__
+
+/* -------------------------------------------------------------------------- */
+#include "compute.hh"
+#include "compute_interaction.hh"
+#include "matrix.hh"
+#include "my_types.hh"
+#include <functional>
+
+//! Compute contact interaction between ping-pong balls
+class ComputeTemperature : public Compute {
+
+  // Virtual implementation
+public:
+  ComputeTemperature();
+  ~ComputeTemperature() override = default;
+  
+  //! Penalty contact implementation
+  void compute(System& system) override;
+
+  Real Rho;               // Mass density
+  Real HeatCapacity;      // Heat capacity
+  Real HeatConductivity;  // Heat Conductivity
+  Real DeltaT;            // Time step for integration
+};
+
+/* -------------------------------------------------------------------------- */
+#endif  //__COMPUTE_TEMPERATURE__HH__
