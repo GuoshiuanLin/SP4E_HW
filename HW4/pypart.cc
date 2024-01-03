3c825c5fb8ec1eb636aeef20f08d9dace97c1a5d
GuoshiuanLin <135812841+GuoshiuanLin@users.noreply.github.com>
2024-01-03 15:55:15 +0100

    Updated pypart.cc still error when running python main.py

--- HW4/pypart.cc
+++ HW4/pypart.cc
@@ -1,5 +1,4 @@
 #include <pybind11/pybind11.h>
-#include <pybind11/functional.h>
 
 namespace py = pybind11;
 
@@ -14,14 +13,20 @@ namespace py = pybind11;
 #include "compute_temperature.hh"
 #include "compute_verlet_integration.hh"
 #include "particles_factory_interface.hh"
-#include "system.hh"
-#include "system_evolution.hh"
 
 PYBIND11_MODULE(pypart, m) {
 
   m.doc() = "pybind of the Particles project";
 
   // bind the routines here
+  // Bind CsvWriter class
+    py::class_<CsvWriter>(m, "CsvWriter")
+        .def(py::init<const std::string&>())
+        .def("write", &CsvWriter::write);
+  // Bind Compute class
+    py::class_<Compute>(m, "Compute");
+        // .def(py::init<>())
+        // .def("compute", &Compute::compute);
   // Bind ComputeTemperature and make the private members accessible
     py::class_<ComputeTemperature>(m, "ComputeTemperature")
         .def(py::init<>())
@@ -33,51 +38,26 @@ PYBIND11_MODULE(pypart, m) {
         .def_property("delta_t", &ComputeTemperature::getDeltat, &ComputeTemperature::getDeltat)
         .def_readwrite("implicit", &ComputeTemperature::implicit);
 
-    // Bind CsvWriter class
-    py::class_<CsvWriter>(m, "CsvWriter")
-        .def(py::init<const std::string&>())
-        .def("write", &CsvWriter::write);
-    
-    // // Bind Compute class
-    // py::class_<Compute>(m, "Compute")
-    //     .def(py::init<>())
-    //     .def("compute", &Compute::compute);
-
     // Bind ComputeInteraction class
-    py::class_<ComputeInteraction, Compute>(m, "ComputeInteraction")
-        .def(py::init<>())
-        .def("applyOnPairs", &ComputeInteraction::applyOnPairs);
+    // py::class_<ComputeInteraction, Compute>(m, "ComputeInteraction")
+    //     .def(py::init<>())
+    //     // .def("applyOnPairs", &ComputeInteraction::applyOnPairs);
 
     // Bind ComputeGravity class
     py::class_<ComputeGravity, Compute>(m, "ComputeGravity")
         .def(py::init<>())
-        .def("compute", &ComputeGravity::compute);
+        .def("setG", &ComputeGravity::setG);
+        // .def("compute", &ComputeGravity::compute);
 
     // Bind ComputeVerletIntegration class
     py::class_<ComputeVerletIntegration, Compute>(m, "ComputeVerletIntegration")
-        .def(py::init<>())
-        .def("compute", &ComputeVerletIntegration::compute);
-    
-
-    // Bind MaterialPointsFactory class
-    py::class_<MaterialPointsFactory>(m, "MaterialPointsFactory")
-        .def(py::init<>())
-        .def("getInstance", &MaterialPointsFactory::getInstance);///
-
-    // Bind PingPongBallsFactory class
-    py::class_<PingPongBallsFactory>(m, "PingPongBallsFactory")
-        .def(py::init<>())
-        .def("getInstance", &PingPongBallsFactory::getInstance);///
-
-    // Bind PlanetsFactory class
-    py::class_<PlanetsFactory>(m, "PlanetsFactory")
-        .def(py::init<>())
-        .def("getInstance", &PlanetsFactory::getInstance);///
+        .def(py::init<Real>())
+        .def("addInteraction", &ComputeVerletIntegration::addInteraction);
 
     // Bind ParticlesFactoryInterface class
     py::class_<ParticlesFactoryInterface>(m, "ParticlesFactoryInterface")
-        .def(py::init<>())
-        .def("getInstance", &ParticlesFactoryInterface::getInstance)
+        // .def(py::init<>())
+        .def_static("getInstance", &ParticlesFactoryInterface::getInstance, py::return_value_policy::reference)
         .def("createSimulation",
             py::overload_cast<const std::string&, Real, py::function>(
                 &ParticlesFactoryInterface::createSimulation<py::function>),
@@ -90,30 +70,36 @@ PYBIND11_MODULE(pypart, m) {
             },
             py::return_value_policy::reference);
 
+    // // Bind MaterialPointsFactory class
+    // py::class_<MaterialPointsFactory, ParticlesFactoryInterface, std::unique_ptr<MaterialPointsFactory>>(m, "MaterialPointsFactory")
+    //     .def_static("getInstance", &MaterialPointsFactory::getInstance);
+
+    // // Bind PingPongBallsFactory class
+    // py::class_<PingPongBallsFactory, ParticlesFactoryInterface, std::unique_ptr<PingPongBallsFactory>>(m, "PingPongBallsFactory")
+    //     .def_static("getInstance", &MaterialPointsFactory::getInstance);
+
+    // // Bind PlanetsFactory class
+    // py::class_<PlanetsFactory, ParticlesFactoryInterface, std::unique_ptr<PlanetsFactory>>(m, "PlanetsFactory")
+    //     .def_static("getInstance", &MaterialPointsFactory::getInstance);
+
+    py::class_<MaterialPointsFactory, ParticlesFactoryInterface>(m, "MaterialPointsFactory")
+        .def("getInstance", &MaterialPointsFactory::getInstance, py::return_value_policy::reference);
+    py::class_<PingPongBallsFactory, ParticlesFactoryInterface>(m, "PingPongBallsFactory")
+        .def("getInstance", &PingPongBallsFactory::getInstance, py::return_value_policy::reference);
+    py::class_<PlanetsFactory, ParticlesFactoryInterface>(m, "PlanetsFactory")
+        .def("getInstance", &PlanetsFactory::getInstance, py::return_value_policy::reference);
+
+
+
+
     // Bind SystemEvolution class
     py::class_<SystemEvolution>(m, "SystemEvolution")
-        .def(py::init<std::unique_ptr<System>>())
+        // .def(py::init<std::unique_ptr<System>>())
         .def("evolve", &SystemEvolution::evolve)
-        .def("addCompute", &SystemEvolution::addCompute)
-        .def("setNSteps", &SystemEvolution::setNSteps)
-        .def("setDumpFreq", &SystemEvolution::setDumpFreq)
-        .def("getSystem", &SystemEvolution::getSystem, py::return_value_policy::reference_internal); // Assuming getSystem returns a reference
+        .def("addCompute", &SystemEvolution::addCompute);
+        // .def("setNSteps", &SystemEvolution::setNSteps)
+        // .def("setDumpFreq", &SystemEvolution::setDumpFreq)
+        // .def("getSystem", &SystemEvolution::getSystem, py::return_value_policy::reference_internal); // Assuming getSystem returns a reference
 
     
-}
-
-PYBIND11_MODULE(pypart, m) {
-    m.doc() = "pybind of the Particles project";
-
-    bindComputeTemperature(m);
-    bindCsvWriter(m);
-    bindCompute(m);
-    bindComputeInteraction(m);
-    bindComputeGravity(m);
-    bindComputeVerletIntegration(m);
-    bindParticlesFactory(m);
-    bindMaterialPointsFactory(m);
-    bindPingPongBallsFactory(m);
-    bindPlanetsFactory(m);
-    bindParticlesFactoryInterface(m);
 }
